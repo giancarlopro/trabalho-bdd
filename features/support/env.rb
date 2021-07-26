7692c4c4a2fcf1bb1aeb71e8aedc90a5ad8d43b9
Giancarlo Rocha <giancarloiff@gmail.com>
2021-07-26 16:47:14 -0300

    fix selenium

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,3 +18,16 @@ end
 World do
   MyAppWorld.new
 end
+
+if ENV['HEADLESS'] == 'true'
+  Capybara.register_driver :selenium do |app|
+    browser_options = ::Selenium::WebDriver::Firefox::Options.new()
+    browser_options.args << '--headless'
+
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :firefox,
+      options: browser_options
+    )
+  end
+end
